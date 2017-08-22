f1e3daa0434b5f3e2c85a3204131b4829bb9e321
Julien Bouquillon <julien@revolunet.com>
2017-08-23 01:15:40 +0200

    better demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,33 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import GithubCorner from "react-github-corner";
+import cities from "all-the-cities/cities.json";
+
 import FuzzyInput from "../../src";
 
-const PRODUCTS = [
-  "iPhone 4",
-  "iPhone 5",
-  "iPhone 6",
-  "iPhone 7",
-  "iPad 1",
-  "iPad 2",
-  "iPad 3",
-  "iPad 4",
-  "iPad Air",
-  "iPad Pro",
-  "Motorola G",
-  "Motorola G 2",
-  "Motorola X",
-  "Nexus 1",
-  "Nexus 2",
-  "Nexus 3",
-  "Nexus 4",
-  "Google pixel"
-];
+
 
 const Demo = () => (
   <div>
     <h1>react-fuzzy-input-text</h1>
-    <FuzzyInput placeholder="Search device" items={ PRODUCTS } onChange={ item => alert(item) } />
+    <p>Search city example using <a href="https://github.com/zeke/all-the-cities">all-the-cities</a> dataset</p>
+    <FuzzyInput placeholder="Search city" items={ cities.map(c => c.name) } onChange={ item => alert(item) } />
     <GithubCorner href="http://github.com/revolunet/react-fuzzy-input-text" />
   </div>
 );
